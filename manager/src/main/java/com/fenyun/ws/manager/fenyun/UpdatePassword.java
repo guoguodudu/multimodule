d5ef08f240fb4bd785ac5f238b411b18d0b86fc9
dupihua <pihuadu@everest-fortune.com>
2020-04-30 11:15:31 +0800

    修改用户名，密码

---
+++ manager/src/main/java/com/fenyun/ws/manager/fenyun/UpdatePassword.java
@@ -0,0 +1,25 @@
+package com.fenyun.ws.manager.fenyun;
+
+import cn.hutool.json.JSONUtil;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+
+public class UpdatePassword {
+
+    public final static String HASH_ALGORITHM_NAME = "MD5"; 	//shiro加密算法
+    public final static int HASH_ITERATIONS = 2;
+    public static void main(String[] args) {
+
+        String userName="EF863";
+        String password="123";
+
+        String pwd = new SimpleHash(
+                HASH_ALGORITHM_NAME,
+                password,
+                ByteSource.Util.bytes(userName+"everest"),
+                HASH_ITERATIONS
+        ).toString();
+
+        System.out.println(pwd);
+    }
+}
