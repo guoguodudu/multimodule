915c97a3c011c226b85d74feff2dcb3638c6adef
dupihua <pihuadu@everest-fortune.com>
2020-05-26 17:59:38 +0800

    xitong sheji

--- manager/src/main/java/com/fenyun/ws/manager/demo/ddd.java
+++ manager/src/main/java/com/fenyun/ws/manager/demo/ddd.java
@@ -1,15 +1,23 @@
 package com.fenyun.ws.manager.demo;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class ddd {
-    private final static DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    //private final static DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+
+    @NotNull public static void someMethod( String  someParameter) {
+       // someParameter.equals("");
+        System.out.println("123");
+    }
 
     public static void main(String[] args) {
 
-        System.out.println(LocalDateTime.parse("2020-04-23", df));
+       // System.out.println(LocalDateTime.parse("2020-04-23", df));
 
+        ddd.someMethod(null);
 
     }
 }
--- manager/src/test/java/com/fenyun/ws/manager/service/Impl/user/IUserServiceImplTest.java
+++ manager/src/test/java/com/fenyun/ws/manager/service/Impl/user/IUserServiceImplTest.java
@@ -5,6 +5,7 @@ import com.fenyun.ws.manager.domain.user.User;
 import com.fenyun.ws.manager.mapper.StudentMapper;
 import com.fenyun.ws.manager.mapper.user.UserMapper;
 import com.google.common.collect.Lists;
+import org.apache.commons.collections.ListUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import tk.mybatis.mapper.entity.Example;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.Assert.*;
 
@@ -31,7 +35,38 @@ public class IUserServiceImplTest {
         Example example=new Example(User.class);
         Example.Criteria criteria=example.createCriteria();
       //  criteria.andEqualTo("name","");
-        criteria.andIn("name",strings);
-        System.out.println(userMapper.deleteByExample(example));
+        //criteria.andIn("name",strings);
+        System.out.println(userMapper.selectAll().size());
+
+        List<User> users=userMapper.selectAll();
+     /*   for (int i = 0; i < 100000; i++) {
+            User user=new User();
+            user.setName(String.valueOf(i));
+            users.add(user);
+            userMapper.insert(user);
+        }*/
+        List<List<User>> userlist = Lists.partition(users, 5000);
+        System.out.println(userlist.size());
+
+        ExecutorService executorService=Executors.newFixedThreadPool(1);
+        for (int i = 0; i < 1; i++) {
+            int finalI = i;
+            executorService.execute(()->{
+
+                System.out.println(userlist.get(finalI).size());
+                for (User user:
+                userlist.get(finalI)) {
+                    System.out.println(Thread.currentThread().getName()+"---------"+finalI);
+                    System.out.println(user);
+                    updateUser(user);
+                }
+
+            });
+        }
+       // executorService.shutdown();
+
+    }
+    private void updateUser(User user){
+        userMapper.updateByPrimaryKey(user);
     }
 }
